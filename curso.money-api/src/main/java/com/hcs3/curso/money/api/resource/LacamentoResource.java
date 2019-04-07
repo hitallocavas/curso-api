1dba009eb824710fd81adb0fbce0a686f379d7ba
hitallocavas <hcs3@cin.ufpe.br>
2019-04-07 13:27:03 -0300

    Deleting Lancamento by code

--- curso.money-api/src/main/java/com/hcs3/curso/money/api/resource/LacamentoResource.java
+++ curso.money-api/src/main/java/com/hcs3/curso/money/api/resource/LacamentoResource.java
@@ -15,12 +15,14 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcs3.curso.money.api.eventos.RecursoCriadoEvent;
@@ -58,6 +60,13 @@ public class LacamentoResource {
 	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter){
 		return lancamentoRepository.filtrar(lancamentoFilter);
 		
+	}
+	
+	@DeleteMapping("/{codigo}")
+	public ResponseEntity<Object> remover(@PathVariable Long codigo){
+		Lancamento lancamentoToRemove = this.getLancamento(codigo);
+		this.lancamentoRepository.delete(lancamentoToRemove);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Lancamento removido com sucesso");
 	}
 	
 	
